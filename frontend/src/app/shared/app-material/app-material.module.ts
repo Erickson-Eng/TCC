ecccfed4a2acd0f7dcb8f74f78f277a359ba3c3e
Erickson Azevedo <erickson.tulio96@gmail.com>
2023-03-16 20:42:15 -0300

    Finalização da tela de login, necessário implementar a lógica

--- frontend/src/app/shared/app-material/app-material.module.ts
+++ frontend/src/app/shared/app-material/app-material.module.ts
@@ -3,6 +3,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import {MatInputModule} from '@angular/material/input';
     MatToolbarModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
 })
 export class AppMaterialModule { }
